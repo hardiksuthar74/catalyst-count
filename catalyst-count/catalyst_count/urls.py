12a615d47b8fb1df539e352cf0ce379df74a7f8f
hardiksuthar74 <sutharhardik747@gmail.com>
2024-12-02 10:07:41 +0530

    added url

--- catalyst-count/catalyst_count/urls.py
+++ catalyst-count/catalyst_count/urls.py
@@ -1,11 +1,18 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from django.shortcuts import redirect
+from django.contrib.auth.decorators import login_required
+
+
+@login_required
+def goToLogin(request):
+    return redirect('/app/company')
+
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("app/", include("app.urls")),
     path("accounts/", include("allauth.urls")),
+    path("", goToLogin),
 ]
-
-
-handler404 = "app.views.redirect_to_login"
